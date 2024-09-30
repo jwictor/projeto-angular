9ddfb631f8345aac50085b3ae07a82bdd0d00f3c
Joao Wictor <joaowictoroliveira3301@gmail.com>
2024-09-30 19:57:02 -0300

    chore: add rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import {
-  PoMenuItem,
-  PoMenuModule,
-  PoPageModule,
-  PoToolbarModule,
-} from '@po-ui/ng-components';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
     RouterOutlet,
-    PoToolbarModule,
-    PoMenuModule,
-    PoPageModule
   ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <router-outlet/>
+  `
 })
-export class AppComponent {
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
-  ];
-
-  private onClick() {
-    alert('Clicked in menu item');
-  }
-}
+export class AppComponent {}
