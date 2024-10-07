a1ec91d67eae15e66816280a4e7207fdf48b3ffc
Joao Wictor <joaowictoroliveira3301@gmail.com>
2024-10-07 19:02:08 -0300

    atualizacao page master

--- src/app/paginas/masterpage/masterpage.component.ts
+++ src/app/paginas/masterpage/masterpage.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {  RouterModule } from '@angular/router';
+import { PoMenuModule, PoMenuPanelItem, PoMenuPanelModule, PoPageModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-masterpage',
   standalone: true,
-  imports: [],
+  imports: [PoMenuModule,PoMenuPanelModule,PoPageModule,RouterModule],
   templateUrl: './masterpage.component.html',
   styleUrl: './masterpage.component.css'
 })
 export class MasterpageComponent {
 
+    title: string = 'Home'
+
+    readonly menus: PoMenuPanelItem[] = [
+      {label: 'Home', link: 'home', action: this.clickItemMenu.bind(this), icon: 'po-icon po-icon-home'},
+      {label: 'Customers', link: 'customers', action: this.clickItemMenu.bind(this), icon: 'po-icon po-icon-user'},
+      {label: 'Products', link: 'catalog', action: this.clickItemMenu.bind(this), icon: 'po-icon po-icon-grid'},
+      {label: 'Budget', link: 'budgets', action: this.clickItemMenu.bind(this), icon: 'po-icon po-icon-pushcart'},
+      {label: 'Exit', link: 'logoff', action: this.clickItemMenu.bind(this), icon: 'po-icon po-icon-exit'}
+    ]
+
+    clickItemMenu(menu: PoMenuPanelItem):void {
+
+    }
 }
